c78d3bde4b85a657a48ee3c10589ce54f674c58d
KITO, takayuki <takayuki.kito@uhero.co.jp>
2015-03-21 22:23:34 +0900

    Add preload (IE: mp3, others: ogg)

--- app/js/app.js
+++ app/js/app.js
@@ -20,6 +20,11 @@ function main() {
 
     enchant();
     var core = new Core(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+    ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"].map(function (filename) {
+        var ext = (enchant.ENV.BROWSER === "ie") ? ".mp3" : ".ogg";
+        var assetName = "sound/" + filename + ext;
+        core.preload(assetName);
+    });
     core.preload("img/keys.png");
     core.fps = 60;
     core.onload = function () {
